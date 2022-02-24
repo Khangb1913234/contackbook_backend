631126999e5ea1259c040121cfe5d044465bea0c
DangVuDinhKhang <khangb1913234@student.ctu.edu.vn>
2022-02-25 01:10:11 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+const { BadRequestError, errorHandler } = require("./app/errors");
 
 const app = express();
 
@@ -10,6 +11,19 @@ app.use(express.urlencoded({ extended: true }));
 
 const setupContactRoutes = require("./app/routes/contact.routes");
 setupContactRoutes(app);
+
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung.
+    // Trong các đoạn code xử lý ở các route, gọi next(error)
+    // sẽ chuyển về middleware xử lý lỗi này
+    errorHandler.handleError(err, res);
+});
 
 app.get('/', function(req, res){
     res.json({ message: 'Welcome to contact book application' });
